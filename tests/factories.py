c4bf5ebd485b77718f51ebf69c4d4bb371d334e8
SuhrutHeroorkar <suhrutheroorkar@gmail.com>
2024-04-29 01:16:17 -0400

    tdd bdd final project

--- tests/factories.py
+++ tests/factories.py
@@ -32,3 +32,8 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone_number = factory.Faker("phone_number")
+    disabled = FuzzyChoice(choices=[True, False])
+    date_joined = FuzzyDate(date(2008, 1, 1))
